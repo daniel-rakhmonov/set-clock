4b42aba307e8a82513284b9637302b91e9127af0
daniel-rakhmonov <61292750+daniel-rakhmonov@users.noreply.github.com>
2020-02-20 19:59:58 -0300

    Add files via upload

---
+++ set_clock.py
@@ -0,0 +1,23 @@
+#!venv/bin/python3
+from bs4 import BeautifulSoup
+from os import system
+import urllib.request
+
+url = 'http://worldtimeapi.org'
+
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page, 'html.parser')
+list_item = soup.find('div', attrs={'class': 'result'})
+name = list_item.text.strip()
+name2 = name.split()[16]
+
+mo = name2[5:7]
+da = name2[8:10]
+hr = name2[11:13]
+mi = name2[14:16]
+ye = name2[0:4]
+se = name2[17:19]
+
+exec1 = system('date %s%s%s%s%s.%s' % (mo,da,hr,mi,ye,se))
+
+
